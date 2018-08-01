92f7431cf757d334af742a88e402147e36aba140
Elizabeth <avrora7@yahoo.com>
2018-08-01 00:06:33 -0700

    working code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 console.log(Math.random().toString(27).substring(2, 15))
 
-const letters = ["a", "b", "c"]
+const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 let computerChoice;
 let userGuesses;
 let wins = 0;
@@ -9,6 +9,13 @@ let attemptsLeft;
 gameReset()
 updateScreen()
 
+document.onkeyup = function(event) {
+    let placeInArray = letters.indexOf(event.key)
+    
+    if (placeInArray > -1) {
+        handleSelection(event.key);
+    }
+}
 
 function handleSelection(key) {
     if (key === computerChoice) {
@@ -19,7 +26,7 @@ function handleSelection(key) {
         attemptsLeft--
         userGuesses.push(key)
         
-        if (attemptsLeft = 0) {
+        if (attemptsLeft === 0) {
             losses++
             gameReset()
         }
@@ -29,13 +36,13 @@ function handleSelection(key) {
 }
 
 function updateScreen() {
-    let output = "Wins: " + wins + "<br>Losses: " + losses + "<br>Guesses left: " + attemptsLeft + "<br>Your guesses so far: " + userGuesses
+    let output = "Wins: " + wins + "<br>Losses: " + losses + "<br>Guesses left: " + attemptsLeft + "<br>Your guesses so far: " + userGuesses.join()
     document.querySelector("#game").innerHTML = output;
 }
 
 function gameReset() {
     attemptsLeft = 9
-    userGuess = []
+    userGuesses = []
     computerChoice = letters[Math.floor(Math.random() * letters.length)]
 }
 
